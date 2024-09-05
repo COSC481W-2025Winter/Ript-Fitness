deb1418de90cb2267b605ddc60174ed791f1896d
Nathan Halash <nhalash@emich.edu>
2024-09-05 17:28:17 -0400

    added some GET and POST method examples

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/TestRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/TestRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.model.TestModel;
 
-public interface TestRepository extends JpaRepository <TestModel, Long>{
+// The Jpa Repository has CRUD methods such as save(), delete() and findById()
+
+// "<TestModel, Long>" represent the type that the repository will handle (TestModel) and the type of the 
+//		primary key (Long). Each TestModel can be uniquely identified by a Long ID.
+
+
+public interface TestRepository extends JpaRepository <TestModel, Long> { 
 	//Put database stuff in here!
 
 }
