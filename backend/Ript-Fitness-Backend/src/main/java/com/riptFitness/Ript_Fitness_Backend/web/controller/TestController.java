dbc31ae034cde1d48ee4553b9cf94fcf61894f5b
Nathan Halash <nhalash@emich.edu>
2024-09-09 14:09:50 -0400

    added a put method to edit DB objects

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/web/controller/TestController.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/web/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,16 @@ public class TestController {
 		return new ResponseEntity<>(savedTestDto, HttpStatus.CREATED);
 	}
 	
-	// Below is a PUT 
+	// Below is a PUT test for editing an object in the database (first/last name)
+	@PutMapping("/editNameTest")
+	public ResponseEntity<String> editNameTest(@RequestBody TestDto testDto) {
+		// Call the service method to edit the object:
+		TestDto editedTestDto = testService.editFirstLast(testDto);
+		// Create a string representation of the object:
+		String updatedDto = "Id: " + editedTestDto.getId() + "\n" + "First Name: " + editedTestDto.getFirstName() 
+								+ "\n" + "Last Name: " + editedTestDto.getLastName(); 
+		return new ResponseEntity<>(updatedDto, HttpStatus.OK);
+	}
 }
 
 
