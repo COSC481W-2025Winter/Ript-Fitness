291a8a70c251e496f049ae2d9a78865d614c39d5
Chris Pichler <cpichle1@emich.edu>
2024-09-18 13:28:11 -0400

    Added application-test.properties to .gitignore, changed default test class to check for application-test.properties

--- backend/Ript-Fitness-Backend/src/test/java/com/riptFitness/Ript_Fitness_Backend/RiptFitnessBackendApplicationTests.java
+++ backend/Ript-Fitness-Backend/src/test/java/com/riptFitness/Ript_Fitness_Backend/RiptFitnessBackendApplicationTests.java
@@ -2,7 +2,9 @@ package com.riptFitness.Ript_Fitness_Backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class RiptFitnessBackendApplicationTests {
 
