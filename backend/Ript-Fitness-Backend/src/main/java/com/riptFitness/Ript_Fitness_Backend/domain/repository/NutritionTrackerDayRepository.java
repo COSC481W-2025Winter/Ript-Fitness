733ad8a8eb271c0f4c6a348f3300d702e051e94b
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-09-25 14:29:33 -0400

    Added findById and save queries to the Day repository. Testing to see if the List values will work

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/NutritionTrackerDayRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/NutritionTrackerDayRepository.java
@@ -1,12 +1,39 @@
 package com.riptFitness.Ript_Fitness_Backend.domain.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.model.Day;
+import com.riptFitness.Ript_Fitness_Backend.domain.model.Food;
+
+import jakarta.transaction.Transactional;
 
 public interface NutritionTrackerDayRepository extends JpaRepository<Day, Long>{
 	//Queries needed: 
 		//Optional<Day> findById(Long dayId): Looks in database for an ID equal to "dayId". Returns Optional<Day> object equal to that row if exists. Returns empty Optional<Day> object if it does not exist in DB.
 		//Day save(Day dayModel): Saves dayModel to "Dahy" database table
+	
+	@Override
+	@Query("SELECT d FROM Day d WHERE d.dayId = :dayId AND f.isDeleted = false")
+	Optional<Day> findById(Long dayId);
+	
+	 @Modifying
+	 @Transactional
+	 @Query(value = "INSERT INTO Day (calories, foodIdsInFoodsEatenInDayList, foodsEatenInDay, isDeleted, totalCarbs, totalFat, totalProtein) VALUES (:calories, :foodIdsInFoodsEatenInDayList, :foodsEatenInDay, :id, :isDeleted, :totalCarbs, :totalFat, :totalProtein)", nativeQuery = true)
+	 void save(@Param("calories") double calories, 
+			   @Param("totalCarbs") double totalCarbs,
+			   @Param("totalProtein") double totalProtein,
+			   @Param("totalFat") double totalFat,
+			   @Param("isDeleted") boolean isDeleted, 
+			   
+			   @Param("foodIdsInFoodsEatenInDayList") List<Long> foodIdsInFoodsEatenInDayList; 
+			   //@Param("foodsEatenInDay") int foodsEatenInDay);
+	
+
 }
