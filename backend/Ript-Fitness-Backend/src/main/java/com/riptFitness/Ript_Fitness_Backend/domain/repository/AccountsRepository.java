6b0ea7a77c106c571505f6b5e432479df6e2cea8
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-09-30 14:00:53 -0400

    Modified the 4 queries for the AccountsRepository.

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/AccountsRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/AccountsRepository.java
@@ -15,26 +15,42 @@ import jakarta.transaction.Transactional;
 
 public interface AccountsRepository extends JpaRepository <AccountsModel, Long> { 
 	
-	// Q1. I need a query that saves an AccountsModel object into the accounts_model table given that the user enteres a username, password, and email
-	public AccountsModel saveAccountModel(String username, String password, String email, LocalDateTime loginDate);
+	// Query that saves an AccountsModel object into the accounts_model table given that the user enters a username, password, and email
+	@Modifying
+    @Query(value = "INSERT INTO accounts_model (username, password, email, loginDate) VALUES (:username, :password, :email)", nativeQuery = true)
+    void saveAccountModel(@Param("username") String username, @Param("password") String password, @Param("email") String email, @Param("loginDate") LocalDateTime loginDate );
+	
+	//public AccountsModel saveAccountModel(String username, String password, String email, LocalDateTime loginDate);
 	
 	// ---------------------------------------------------------------------------------------------------------
 
-	// Q2. I need a query to be written for the below method which returns true or false depending on wether the given username is in the accounts_model database table
-	public boolean existsByUsername(String username); // (your query will replace this method)
+	// Query which returns true or false depending on whether the given username is in the accounts_model database table
+	//public boolean existsByUsername(String username); // (your query will replace this method)
+	@Query(value = "SELECT COUNT(*) > 0 FROM accounts_model WHERE username = :username", nativeQuery = true)
+    boolean existsByUsername(@Param("username") String username);
+	
 	
 	// ---------------------------------------------------------------------------------------------------------
     
-	// Q3. I need a query here that gets the ID based off of the username that is entered in the login page from the accounts_model table in DB
-    @Query("")
+	// Query that gets the ID based off of the username that is entered in the login page from the accounts_model table in DB
+    @Query(value = "SELECT id FROM accounts_model WHERE username = :username", nativeQuery = true)
     Optional<Long> findIdByUsername(@Param("username") String username);
+    
 
     // ---------------------------------------------------------------------------------------------------------
     
-    // Q4. I need a query that resets 'lastLogin' in the database every time that a login has been made (UPDATE) based off of username.
+    // Query that resets 'lastLogin' in the database every time that a login has been made (UPDATE) based off of username.
     @Modifying // This is neccesary because we are modifying the database instead of creating something new
     @Transactional // This is required for modifying queries
-    @Query("")
+    @Query(value = "UPDATE accounts_model SET last_login = CURRENT_TIMESTAMP WHERE username = :username", nativeQuery = true)
     void updateLoginDate(@Param("username") String username, @Param("lastLogin") LocalDateTime lastLogin);
+    
+    
+    
+    //@Query("")
+    //void updateLoginDate(@Param("username") String username, @Param("lastLogin") LocalDateTime lastLogin);
 
+  
+    
+    
 }
