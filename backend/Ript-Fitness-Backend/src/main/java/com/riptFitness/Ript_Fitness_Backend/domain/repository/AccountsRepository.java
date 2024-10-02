18ffdc4df15234b4f5071c974c921471d074f74c
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-10-02 13:50:32 -0400

    style: edited the AccountsRepository queries and eliminated unecessary comments.

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/AccountsRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/AccountsRepository.java
@@ -19,39 +19,19 @@ public interface AccountsRepository extends JpaRepository <AccountsModel, Long> 
 	@Modifying
     @Query(value = "INSERT INTO accounts_model (username, password, email, loginDate) VALUES (:username, :password, :email, :loginDate)", nativeQuery = true)
     void saveAccountModel(@Param("username") String username, @Param("password") String password, @Param("email") String email, @Param("loginDate") LocalDateTime loginDate);
-	
-	//public AccountsModel saveAccountModel(String username, String password, String email, LocalDateTime loginDate);
-	
-	// ---------------------------------------------------------------------------------------------------------
 
 	// Query which returns true or false depending on whether the given username is in the accounts_model database table
-	//public boolean existsByUsername(String username); // (your query will replace this method)
 	@Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM accounts_model WHERE username = :username", nativeQuery = true)
 	Long existsByUsername(@Param("username") String username);
-
-	
-	
-	// ---------------------------------------------------------------------------------------------------------
     
 	// Query that gets the ID based off of the username that is entered in the login page from the accounts_model table in DB
     @Query(value = "SELECT id FROM accounts_model WHERE username = :username", nativeQuery = true)
     Optional<Long> findIdByUsername(@Param("username") String username);
     
-
-    // ---------------------------------------------------------------------------------------------------------
-    
     // Query that resets 'lastLogin' in the database every time that a login has been made (UPDATE) based off of username.
-    @Modifying // This is neccesary because we are modifying the database instead of creating something new
-    @Transactional // This is required for modifying queries
+    @Modifying // Modifying the database instead of creating something new
+    @Transactional // Required for modifying queries
     @Query(value = "UPDATE accounts_model SET last_login = :lastLogin WHERE username = :username", nativeQuery = true)
     void updateLoginDate(@Param("username") String username, @Param("lastLogin") LocalDateTime lastLogin);
-    
-    
-    
-    //@Query("")
-    //void updateLoginDate(@Param("username") String username, @Param("lastLogin") LocalDateTime lastLogin);
-
-  
-    
     
 }
