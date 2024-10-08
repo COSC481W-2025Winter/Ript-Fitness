3a5449eb845e6fdc0b061e756204daa5c63bb473
Evan <no@email.com>
2024-10-08 16:52:59 -0400

    Login Testing

---
+++ frontend/jest.setup.js
@@ -0,0 +1,42 @@
+// jest.setup.js
+jest.mock('expo-modules-core', () => ({
+    NativeModule: jest.fn(),
+  }));
+  
+  jest.mock('expo-font', () => ({
+    loadAsync: jest.fn(),
+  }));
+  
+  jest.mock('@expo/vector-icons', () => ({
+    Ionicons: 'Ionicons',
+  }));
+  
+  jest.mock('@react-navigation/native', () => {
+    return {
+      ...jest.requireActual('@react-navigation/native'),
+      useNavigation: () => ({
+        navigate: jest.fn(),
+      }),
+    };
+  });
+
+  jest.mock('react-native-gesture-handler', () => {
+    const View = require('react-native').View;
+    return {
+      ScrollView: View,
+      Switch: View,
+      TextInput: View,
+      DrawerLayoutAndroid: View,
+      WebView: View,
+      State: {},
+      PanGestureHandler: View,
+      TapGestureHandler: View,
+      FlingGestureHandler: View,
+      ForceTouchGestureHandler: View,
+      LongPressGestureHandler: View,
+      PinchGestureHandler: View,
+      RotationGestureHandler: View,
+      /* Add any other components as needed */
+    };
+  });
+  
