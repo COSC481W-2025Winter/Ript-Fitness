ff7e365accc402d34d692ac9fc1c4cbe8edb3195
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-10-28 15:01:44 -0400

    Query: Added the queries for findByAccountIdAndNotDeleted and findById.

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/ExerciseRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/ExerciseRepository.java
@@ -5,19 +5,22 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.model.AccountsModel;
 import com.riptFitness.Ript_Fitness_Backend.domain.model.ExerciseModel;
+import com.riptFitness.Ript_Fitness_Backend.domain.model.UserProfile;
 
 public interface ExerciseRepository extends JpaRepository <ExerciseModel, Long> {
 	
 	// Query to retrieve a list of type exercises by account_id where isDeleted is false
-    @Query("")
-    List<ExerciseModel> findByAccountIdAndNotDeleted(Long currentUserId);
+    @Query("SELECT e FROM ExerciseModel e WHERE e.currentUserId = :currentUserId AND e.isDeleted = false")
+    List<ExerciseModel> findByAccountIdAndNotDeleted(@Param("currentUserId") Long currentUserId);
     
     // Query to retrieve an account model (someones account) given a current user Id that is obtained via JWT token
-    @Query("")
-    Optional<ExerciseModel> findById(Long currentUserId);
+    @Query("SELECT a FROM AccountsModel a WHERE a.id = :currentUserId")
+    Optional<ExerciseModel> findById(@Param("currentUserId") Long currentUserId);
+    
 }
 
 
