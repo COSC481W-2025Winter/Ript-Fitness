311e52e41b6ec8038b7ca22ea47738658b0b99d6
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-10-29 15:03:02 -0400

    Query: Added the findById queries to SocialPostCommentRepository and SocialPostRepository.

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/SocialPostRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/SocialPostRepository.java
@@ -1,9 +1,20 @@
 package com.riptFitness.Ript_Fitness_Backend.domain.repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.model.SocialPost;
+import com.riptFitness.Ript_Fitness_Backend.domain.model.Streak;
 
 public interface SocialPostRepository extends JpaRepository<SocialPost, Long>{
 	
+	//Searches database for SocialPost row with ID = socialPostId (input parameter) and returns it. If it is found Return the row. If not return an empty Optional object.
+		@Override	
+		@Query("SELECT s FROM SocialPost s WHERE s.socialPostId = :socialPostId")
+		Optional<SocialPost> findById(@Param("socialPostId") Long socialPostId);
+
 }
