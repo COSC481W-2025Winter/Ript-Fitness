d2f8750da0e59de81ced4ab95f4085fffcff3518
Chris Pichler <cpichle1@emich.edu>
2024-10-28 21:39:46 -0400

    Add service methods to be called by Controller endpoints
    1) Allows for all business logic to be defined so that Controller endpoints can call Service methods to perform the business logic necessary to complete the feature
    
    Issue #47

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/infrastructure/service/SocialPostService.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/infrastructure/service/SocialPostService.java
@@ -1,9 +1,14 @@
 package com.riptFitness.Ript_Fitness_Backend.infrastructure.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.mapper.SocialPostMapper;
+import com.riptFitness.Ript_Fitness_Backend.domain.model.AccountsModel;
 import com.riptFitness.Ript_Fitness_Backend.domain.model.SocialPost;
+import com.riptFitness.Ript_Fitness_Backend.domain.model.SocialPostComment;
+import com.riptFitness.Ript_Fitness_Backend.domain.repository.AccountsRepository;
 import com.riptFitness.Ript_Fitness_Backend.domain.repository.SocialPostCommentRepository;
 import com.riptFitness.Ript_Fitness_Backend.domain.repository.SocialPostRepository;
 import com.riptFitness.Ript_Fitness_Backend.web.dto.SocialPostDto;
@@ -14,15 +19,152 @@ public class SocialPostService {
 	private SocialPostRepository socialPostRepository;
 	
 	private SocialPostCommentRepository socialPostCommentRepository;
+	
+	private AccountsRepository accountsRepository;
 
-	public SocialPostService(SocialPostRepository socialPostRepository, SocialPostCommentRepository socialPostCommentRepository) {
+	public SocialPostService(SocialPostRepository socialPostRepository, SocialPostCommentRepository socialPostCommentRepository, AccountsRepository accountsRepository) {
 		this.socialPostRepository = socialPostRepository;
 		this.socialPostCommentRepository = socialPostCommentRepository;
+		this.accountsRepository = accountsRepository;
 	}
 	
 	public SocialPostDto addPost(SocialPostDto socialPostDto) {
 		SocialPost socialPostToBeAdded = SocialPostMapper.INSTANCE.toSocialPost(socialPostDto);
 		socialPostToBeAdded = socialPostRepository.save(socialPostToBeAdded);
 		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostToBeAdded);
+	}
+	
+	public SocialPostDto getPost(Long socialPostId) {
+		Optional<SocialPost> returnedOptionalSocialPostObject = socialPostRepository.findById(socialPostId);
+		
+		if(returnedOptionalSocialPostObject.isEmpty())
+			throw new RuntimeException("SocialPost object not found in database with ID = " + socialPostId);
+		
+		SocialPost returnedSocialPostObject = returnedOptionalSocialPostObject.get();
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(returnedSocialPostObject);
+	}
+	
+	public SocialPostDto editPostContent(Long socialPostId, String newSocialPostContent) {
+		Optional<SocialPost> optionalSocialPostToBeEdited = socialPostRepository.findById(socialPostId);
+		
+		if(optionalSocialPostToBeEdited.isEmpty())
+			throw new RuntimeException("SocialPost object not found in database with ID = " + socialPostId);
+				
+		SocialPost socialPostToBeEdited = optionalSocialPostToBeEdited.get();
+		
+		socialPostToBeEdited.content = newSocialPostContent;
+		
+		socialPostToBeEdited = socialPostRepository.save(socialPostToBeEdited);
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostToBeEdited);
+	}
+	
+	public SocialPostDto deletePost(Long socialPostId) {
+		Optional<SocialPost> optionalSocialPostToBeDeleted = socialPostRepository.findById(socialPostId);
+		
+		if(optionalSocialPostToBeDeleted.isEmpty())
+			throw new RuntimeException("SocialPost object not found in database with ID = " + socialPostId);
+
+		SocialPost socialPostToBeDeleted = optionalSocialPostToBeDeleted.get();
+		
+		socialPostToBeDeleted.isDeleted = true;
+		
+		socialPostToBeDeleted = socialPostRepository.save(socialPostToBeDeleted);
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostToBeDeleted);
+	}
+	
+	public SocialPostDto addLike(Long socialPostId, Long userId) {
+		Optional<SocialPost> optionalSocialPostObject = socialPostRepository.findById(socialPostId);
+		
+		if(optionalSocialPostObject.isEmpty())
+			throw new RuntimeException("SocialPost object not found in database with ID = " + socialPostId);
+
+		Optional<AccountsModel> accountsModelObject = accountsRepository.findById(userId);
+		
+		if(accountsModelObject.isEmpty())
+			throw new RuntimeException("AccountsModel object not found in database with UserID = " + userId);
+		
+		SocialPost socialPostObject = optionalSocialPostObject.get();
+		
+		if(socialPostObject.userIDsOfLikes.contains(userId))
+			throw new RuntimeException("The user with user ID = " + userId + " already liked the SocialPost with ID = " + socialPostId + ". No changes were made to the SocialPost.");
+		
+		socialPostObject.userIDsOfLikes.add(userId);
+		
+		socialPostObject.numberOfLikes++;
+		
+		socialPostObject = socialPostRepository.save(socialPostObject);
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostObject);
+	}
+	
+	public SocialPostDto deleteLike(Long socialPostId, Long userId) {
+		Optional<SocialPost> optionalSocialPostObject = socialPostRepository.findById(socialPostId);
+		
+		if(optionalSocialPostObject.isEmpty())
+			throw new RuntimeException("SocialPost object not found in database with ID = " + socialPostId);
+
+		Optional<AccountsModel> accountsModelObject = accountsRepository.findById(userId);
+		
+		if(accountsModelObject.isEmpty())
+			throw new RuntimeException("AccountsModel object not found in database with UserID = " + userId);
+		
+		SocialPost socialPostObject = optionalSocialPostObject.get();
+		
+		if(!socialPostObject.userIDsOfLikes.contains(userId))
+			throw new RuntimeException("The user with user ID = " + userId + " has not already liked the SocialPost with ID = " + socialPostId + ". No changes were made to the SocialPost.");
+	
+		socialPostObject.userIDsOfLikes.remove(userId);
+		
+		socialPostObject.numberOfLikes--;
+		
+		socialPostRepository.save(socialPostObject);
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostObject);
+	}
+	
+	public SocialPostDto addComment(SocialPostComment socialPostComment) {
+		Optional<SocialPost> optionalSocialPostObject = socialPostRepository.findById(socialPostComment.postId);
+		
+		if(optionalSocialPostObject.isEmpty())
+			throw new RuntimeException("SocialPost object not found in database with ID = " + socialPostComment.postId);
+		
+		SocialPost socialPostObject = optionalSocialPostObject.get();
+				
+		socialPostObject.socialPostComments.add(socialPostComment);
+		
+		socialPostObject = socialPostRepository.save(socialPostObject);
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostObject);
+	}
+	
+	public SocialPostDto deleteComment(Long socialPostCommentId) {
+		Optional<SocialPostComment> optionalSocialPostCommentObject = socialPostCommentRepository.findById(socialPostCommentId);
+		
+		if(optionalSocialPostCommentObject.isEmpty())
+			throw new RuntimeException("SocialPostComment object not found in database with ID = " + socialPostCommentId);
+		
+		SocialPostComment socialPostCommentObject = optionalSocialPostCommentObject.get();
+		
+		socialPostCommentObject.isDeleted = true;
+		
+		SocialPost socialPostContainingDeletedComment = socialPostRepository.findById(socialPostCommentObject.postId).get();
+		
+		SocialPostComment socialPostCommentToBeDeleted = null;
+		
+		for(SocialPostComment socialPostComment : socialPostContainingDeletedComment.socialPostComments) {
+			if(socialPostComment.id == socialPostCommentId)
+				socialPostCommentToBeDeleted = socialPostComment;
+		}
+		
+		socialPostContainingDeletedComment.socialPostComments.remove(socialPostCommentToBeDeleted);
+		
+		socialPostRepository.save(socialPostContainingDeletedComment);
+						
+		socialPostCommentObject = socialPostCommentRepository.save(socialPostCommentObject);
+		
+		return SocialPostMapper.INSTANCE.toSocialPostDto(socialPostContainingDeletedComment);
 	}
 }
