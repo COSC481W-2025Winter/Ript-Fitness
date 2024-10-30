7ffef3b9f4c79c2318892950684b8dc72b2002bc
Chris Pichler <cpichle1@emich.edu>
2024-10-30 14:29:48 -0400

    Edited Repository classes to check for "id" variables and created a query to return all messages sent by a User
    1) Made sure all findById queries are searching for a variable called "id"
    2) Had Tina create  query that returns all message by a specific AccountId
    
    Issue #47

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/SocialPostCommentRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/SocialPostCommentRepository.java
@@ -12,7 +12,7 @@ import com.riptFitness.Ript_Fitness_Backend.domain.model.SocialPostComment;
 public interface SocialPostCommentRepository extends JpaRepository <SocialPostComment, Long>{
 
 	@Override
-	@Query("SELECT d FROM Day d WHERE d.id = :id AND d.isDeleted = false")
-	Optional<SocialPostComment> findById(@Param("socialPostCommentId") Long socialPostCommentId);
+	@Query("SELECT s FROM SocialPostComment s WHERE s.id = :id AND s.isDeleted = false")
+	Optional<SocialPostComment> findById(@Param("id") Long id);
 
 }
--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/SocialPostRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/SocialPostRepository.java
@@ -1,6 +1,7 @@
 package com.riptFitness.Ript_Fitness_Backend.domain.repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,7 +15,9 @@ public interface SocialPostRepository extends JpaRepository<SocialPost, Long>{
 	
 	//Searches database for SocialPost row with ID = socialPostId (input parameter) and returns it. If it is found Return the row. If not return an empty Optional object.
 		@Override	
-		@Query("SELECT s FROM SocialPost s WHERE s.socialPostId = :socialPostId")
-		Optional<SocialPost> findById(@Param("socialPostId") Long socialPostId);
+		@Query("SELECT s FROM SocialPost s WHERE s.id = :id AND s.isDeleted = false")
+		Optional<SocialPost> findById(@Param("id") Long id);
 
+		@Query("SELECT s.id FROM SocialPost s WHERE s.accountId = :accountId AND s.isDeleted = false")
+		Optional<ArrayList<Long>> getPostsFromAccountId(@Param("accountId") Long accountId);
 }
