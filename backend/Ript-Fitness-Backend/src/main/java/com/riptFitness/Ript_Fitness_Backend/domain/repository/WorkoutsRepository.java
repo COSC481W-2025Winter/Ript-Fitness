4fedfa70f397088b1fc5f009c30e0a1c2d4880b0
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-10-30 21:09:26 -0400

    Fix: Fixing the query errors.

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/WorkoutsRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/WorkoutsRepository.java
@@ -17,12 +17,14 @@ import com.riptFitness.Ript_Fitness_Backend.domain.model.Workouts;
 public interface WorkoutsRepository extends JpaRepository <Workouts, Long> {
 
 	//Looks in Workouts table for a record with the inputed ID and that is not deleted. If found, Return the row. If not return  empty Optional object
-    @Override
-    @Query("SELECT i FROM Workouts i WHERE i.workoutsId = :workoutsId AND u.isDeleted = false")
-    Optional<Workouts> findById(@Param("workoutsId") Long workoutsId);
+	@Override
+	@Query("SELECT i FROM Workouts i WHERE i.workoutsId = :workoutsId AND i.isDeleted = false")
+	Optional<Workouts> findById(@Param("workoutsId") Long workoutsId);
+
 	
     //Looks for all workouts with the Id of the current user and returns a list of all that user's workouts
-	@Query("SELECT i FROM Workouts i WHERE i.currentUserId = :currentUserId AND f.isDeleted = false")
+	@Query("SELECT i FROM Workouts i WHERE i.currentUserId = :currentUserId AND i.isDeleted = false")
 	List<Workouts> findByAccountId(@Param("currentUserId") Long currentUserId);
+
 
 }
