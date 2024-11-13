4a0256a221b1fb22df9895804c137087dc3694bd
Nathan Halash <nhalash@emich.edu>
2024-11-13 00:20:57 -0500

    changed token to expire every 50 years

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/infrastructure/config/JwtUtil.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/infrastructure/config/JwtUtil.java
@@ -64,7 +64,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // Token validity: 10 hours
+                .setExpiration(new Date(System.currentTimeMillis() +  1000L * 60 * 60 * 24 * 365 * 50)) // Token validity: 10 hours
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256) // Use the signing key and HS256 algorithm
                 .compact();
     }
