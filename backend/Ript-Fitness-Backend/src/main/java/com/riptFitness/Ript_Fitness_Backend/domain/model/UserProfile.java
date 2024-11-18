b251f7608d5fffcbdafdcd45a02c63d1308dabbd
Tom Van <tvanden3@emich.edu>
2024-11-17 21:10:17 -0500

    #87 | initial commit

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/model/UserProfile.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/model/UserProfile.java
@@ -1,22 +1,19 @@
 package com.riptFitness.Ript_Fitness_Backend.domain.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 
 @Entity
 public class UserProfile {
 
-    @Id //pk
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     @Column(nullable = false)
@@ -24,31 +21,31 @@ public class UserProfile {
 
     @Column(nullable = false)
     public String lastName;
-    
+
     @OneToMany(mappedBy = "userProfile", cascade = CascadeType.ALL, orphanRemoval = true)
     public List<SocialPost> socialPosts;
-    
+
     @OneToMany(mappedBy = "userProfile", cascade = CascadeType.ALL, orphanRemoval = true)
     public List<SocialPostComment> comments;
 
-    //list to store PRs
-    @ElementCollection
-    public List<String> prs = new ArrayList<>();
+    // New bio field
+    @Column
+    public String bio; 
 
-    //user's email or username
+    // User's email or username
     @Column(unique = true, nullable = false)
     public String username;
 
-    //delete (soft delete)
+    // Soft delete flag
     public boolean isDeleted = false;
-    
+
     public UserProfile() {}
 
-    public UserProfile(String firstName, String lastName, String username, List<String> prs) {
+    public UserProfile(String firstName, String lastName, String username, String bio) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
-        this.prs = prs;
+        this.bio = bio;
     }
 
     // Getters and Setters
@@ -76,20 +73,20 @@ public class UserProfile {
         this.lastName = lastName;
     }
 
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public List<String> getPrs() {
-        return prs;
-    }
-
-    public void setPrs(List<String> prs) {
-        this.prs = prs;
     }
 
     public boolean isDeleted() {
