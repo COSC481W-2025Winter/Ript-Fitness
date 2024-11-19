800c850727573649a7029caa6ccdf4eea93ccc91
Chris Pichler <cpichle1@emich.edu>
2024-11-19 15:12:29 -0500

    Add FriendsController.java
    1) Defines HTTP endpoints that the front end will target to make relevant changes to the database
    
    Issue #103

---
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/web/controller/FriendsController.java
@@ -0,0 +1,43 @@
+package com.riptFitness.Ript_Fitness_Backend.web.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riptFitness.Ript_Fitness_Backend.infrastructure.service.FriendsService;
+
+@RestController
+@RequestMapping("/friends")
+public class FriendsController {
+
+	FriendsService friendsService;
+
+	public FriendsController(FriendsService friendsService) {
+		this.friendsService = friendsService;
+	}
+
+	@PostMapping("/addFriend/{id}")
+	public ResponseEntity<String> addFriend(@RequestBody Long id){
+		String returnedString = friendsService.addFriend(id);
+		return new ResponseEntity<>(returnedString, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/getFriendsListOfCurrentlyLoggedInUser")
+	public ResponseEntity<ArrayList<String>> getFriendsListOfCurrentlyLoggedInUser(){
+		ArrayList<String> returnedListOfUsernames = friendsService.getFriendsListOfCurrentlyLoggedInUser();
+		return new ResponseEntity<>(returnedListOfUsernames, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteFriend/{id}")
+	public ResponseEntity<String> deleteFriend(@RequestBody Long id){
+		String returnedString = friendsService.deleteFriend(id);
+		return new ResponseEntity<>(returnedString, HttpStatus.OK);
+	}
+}
