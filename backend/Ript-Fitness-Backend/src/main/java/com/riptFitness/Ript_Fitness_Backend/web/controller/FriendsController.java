69030f40b468c20ef64ac0fd203a0ac2b3c0873a
Chris Pichler <cpichle1@emich.edu>
2024-11-19 22:29:54 -0500

    Changed input parameter of addFriend/deleteFriend endpoints
    1) Changed the input parameters from a request body to a path variable
    
    Issue #103

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/web/controller/FriendsController.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/web/controller/FriendsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class FriendsController {
 	}
 
 	@PostMapping("/addFriend/{id}")
-	public ResponseEntity<String> addFriend(@RequestBody Long id){
+	public ResponseEntity<String> addFriend(@PathVariable Long id){
 		String returnedString = friendsService.addFriend(id);
 		return new ResponseEntity<>(returnedString, HttpStatus.CREATED);
 	}
@@ -36,7 +37,7 @@ public class FriendsController {
 	}
 	
 	@DeleteMapping("/deleteFriend/{id}")
-	public ResponseEntity<String> deleteFriend(@RequestBody Long id){
+	public ResponseEntity<String> deleteFriend(@PathVariable Long id){
 		String returnedString = friendsService.deleteFriend(id);
 		return new ResponseEntity<>(returnedString, HttpStatus.OK);
 	}
