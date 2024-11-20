61f25e5af5ce5062d62eca5f65dc4f5b183d5f4d
Christina Trotta <124186474+ctrotta23@users.noreply.github.com>
2024-11-20 14:25:40 -0500

    Query: Added getStatus query to FriendRequestRepository.

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/FriendRequestRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/FriendRequestRepository.java
@@ -1,9 +1,19 @@
 package com.riptFitness.Ript_Fitness_Backend.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.model.FriendRequest;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long>{
+
+	
+	// Takes in fromAccountId and toAccountId and returns the Status row if a relationship exists between the 2
+	@Query(value = "SELECT f.status FROM FriendRequest f WHERE f.fromAccountId = :fromAccountID AND f.toAccountId = :toAccountId")
+	Optional<String> getStatus(@Param("fromAccountId") Long fromAccountId, @Param("toAccountId") Long toAccountID);
+	
 
 }
