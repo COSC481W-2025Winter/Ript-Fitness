afc46fbd79f9786390f7b9336ae6a331409f5713
Chris Pichler <cpichle1@emich.edu>
2024-11-20 22:35:33 -0500

    Added JPA queries for findBy and existBy for accountIdOfFromAccount and accountIdOfToAccount

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/FriendRequestRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/FriendRequestRepository.java
@@ -1,6 +1,6 @@
 package com.riptFitness.Ript_Fitness_Backend.domain.repository;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,15 +8,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.riptFitness.Ript_Fitness_Backend.domain.model.FriendRequest;
+import com.riptFitness.Ript_Fitness_Backend.domain.model.RequestStatus;
 
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long>{
-
+	//Takes in fromAccountId and toAccountId and returns the Status row if a relationship exists between the 2
+	@Query(value = "SELECT f.status FROM FriendRequest f WHERE f.accountIdOfFromAccount = :fromAccountId AND f.accountIdOfToAccount = :toAccountId")
+	Optional<RequestStatus> getStatus(@Param("fromAccountId") Long fromAccountId, @Param("toAccountId") Long toAccountId);
 	
-	// Takes in fromAccountId and toAccountId and returns the Status row if a relationship exists between the 2
-	@Query(value = "SELECT f.status FROM FriendRequest f WHERE f.fromAccountId = :fromAccountID AND f.toAccountId = :toAccountId")
-	Optional<String> getStatus(@Param("fromAccountId") Long fromAccountId, @Param("toAccountId") Long toAccountID);
+	//Returns true if a relationship exists between the Accounts associated with fromAccountId and toAccountId in the Friend Request database table
+	boolean existsByAccountIdOfFromAccountAndAccountIdOfToAccount(Long fromAccountId, Long toAccountId);
+	
+	//Returns a FriendRequest object representing the row in the Friend Request table with the Accounts associated with fromAccountId and toAccountId (returns null if a relationship doesn't exist)
+	Optional<FriendRequest> findByAccountIdOfFromAccountAndAccountIdOfToAccount(Long fromAccountId, Long toAccountId);
 
-	@Query(value = "SELECT f.toAccountId FROM FriendRequest f WHERE f.fromAccountId = :fromAccountId AND f.status = :status ")
-	List<Long> getToAccountFromFromAccountAndStatus(@Param("fromAccountId") Long fromAccountId, @Param("status") String status);
-
+	//Takes in a fromAccountId and a status String, and returns the toAccountId column of all rows with the Account associated with fromAccountId AND the status equal to the parameter String
+	@Query(value = "SELECT f.accountIdOfToAccount FROM FriendRequest f WHERE f.accountIdOfFromAccount = :fromAccountId AND f.status = :status")
+	ArrayList<Long> getToAccountFromFromAccountAndStatus(@Param("fromAccountId") Long fromAccountId, @Param("status") String status);
 }
