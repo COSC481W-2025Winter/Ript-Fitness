492a59ef4cb3efa93ad1447dc7b9e6ac5808a2c0
Chris Pichler <cpichle1@emich.edu>
2024-12-03 22:55:53 -0500

    Add a One-To-Many relationship in AccountsModel.java between AccountsModel and Graph
    Issue #185

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/model/AccountsModel.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/model/AccountsModel.java
@@ -66,6 +66,9 @@ public class AccountsModel {
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Note> notes = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Graph> graphs;
 
     @ManyToMany
     @JoinTable(
@@ -225,6 +228,30 @@ public class AccountsModel {
 
 	public void setFoods(List<Food> foods) {
 		this.foods = foods;
+	}
+
+	public List<WorkoutData> getWorkoutdata() {
+		return workoutdata;
+	}
+
+	public void setWorkoutdata(List<WorkoutData> workoutdata) {
+		this.workoutdata = workoutdata;
+	}
+
+	public List<Graph> getGraphs() {
+		return graphs;
+	}
+
+	public void setGraphs(List<Graph> graphs) {
+		this.graphs = graphs;
+	}
+
+	public LocalDateTime getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(LocalDateTime lastLogin) {
+		this.lastLogin = lastLogin;
 	}
 	
 	
