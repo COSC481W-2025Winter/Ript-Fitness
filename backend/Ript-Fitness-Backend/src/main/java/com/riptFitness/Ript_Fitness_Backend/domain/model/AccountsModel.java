4781a74ab7003d3a86ce1ce2d1cca6530963ae1a
Chris Pichler <cpichle1@emich.edu>
2024-12-04 13:18:51 -0500

    Deleted a getter and setter for lastLogin that was causing AccountsController unit tests to fail.
    Issue #185

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/model/AccountsModel.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/model/AccountsModel.java
@@ -246,13 +246,6 @@ public class AccountsModel {
 		this.graphs = graphs;
 	}
 
-	public LocalDateTime getLastLogin() {
-		return lastLogin;
-	}
-
-	public void setLastLogin(LocalDateTime lastLogin) {
-		this.lastLogin = lastLogin;
-	}
 	
 	
 }
