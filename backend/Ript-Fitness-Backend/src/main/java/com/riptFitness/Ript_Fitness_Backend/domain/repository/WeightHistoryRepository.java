633ade8bcf3af60da14b5581aa10b7552837157c
Jaison <jeccles1@emich.edu>
2025-02-07 17:30:22 -0500

    Added query to the weightHistoryRepository method findByUserProfileOrderByRecordedAtDesc

--- backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/WeightHistoryRepository.java
+++ backend/Ript-Fitness-Backend/src/main/java/com/riptFitness/Ript_Fitness_Backend/domain/repository/WeightHistoryRepository.java
@@ -11,10 +11,12 @@ import com.riptFitness.Ript_Fitness_Backend.domain.model.WeightHistory;
 
 public interface WeightHistoryRepository extends JpaRepository<WeightHistory, Long>{
 	//return all weight history from newest to oldest
-	List<WeightHistory> findByUserProfileOrderByRecordedAtDesc(Long userId);
+	@Query("SELECT w FROM WeightHistory w WHERE w.userProfile.id = :userProfileId ORDER BY w.recordedAt DESC")
+	List<WeightHistory> findByUserProfileOrderByRecordedAtDesc(@Param("userProfileId") Long userProfileId);
 	
 	//return last recorded weight before the last udpate
 	@Query("SELECT u FROM WeightHistory u WHERE u.userProfile.id = :userId ORDER BY u.recordedAt DESC LIMIT 1")
 	List<WeightHistory> findLastWeightBeforeUpdate (@Param("userId")Long userID);
+
 
 }
