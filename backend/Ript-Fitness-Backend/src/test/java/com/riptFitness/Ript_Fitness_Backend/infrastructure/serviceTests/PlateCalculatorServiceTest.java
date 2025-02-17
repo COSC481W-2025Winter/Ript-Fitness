d8ef3b94e54459404168cd1e0beb938e1719535e
Jaison <jeccles1@emich.edu>
2025-02-17 18:42:03 -0500

    Added test stubs

---
+++ backend/Ript-Fitness-Backend/src/test/java/com/riptFitness/Ript_Fitness_Backend/infrastructure/serviceTests/PlateCalculatorServiceTest.java
@@ -0,0 +1,14 @@
+package com.riptFitness.Ript_Fitness_Backend.infrastructure.serviceTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlateCalculatorServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ backend/Ript-Fitness-Backend/src/test/java/com/riptFitness/Ript_Fitness_Backend/web/controllerTests/PlateCalculatorControllerTest.java
@@ -0,0 +1,14 @@
+package com.riptFitness.Ript_Fitness_Backend.web.controllerTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlateCalculatorControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
